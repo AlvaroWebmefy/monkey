10643002e47a05523b4d8e6d17c480651acbe46d
Alwar <alwaromoreno@gmail.com>
2024-03-27 22:56:50 +0100

    add EQ and NOT_EQ operators; peekChar function to tokenize

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -26,6 +26,14 @@ func (l *Lexer) ReadChar() {
 	l.readPosition += 1
 }
 
+func (l *Lexer) PeekChar() byte {
+	if l.position >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
+}
+
 func (l *Lexer) NextToken() token.Token {
 
 	var tok token.Token
@@ -33,7 +41,14 @@ func (l *Lexer) NextToken() token.Token {
 
 	switch l.ch {
 	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
+		if l.PeekChar() == '=' {
+			ch := l.ch
+			l.ReadChar()
+			literal := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.EQ, Literal: literal}
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
 	case '+':
 		tok = newToken(token.PLUS, l.ch)
 	case '-':
@@ -43,7 +58,14 @@ func (l *Lexer) NextToken() token.Token {
 	case '*':
 		tok = newToken(token.ASTERISK, l.ch)
 	case '!':
-		tok = newToken(token.BANG, l.ch)
+		if l.PeekChar() == '=' {
+			ch := l.ch
+			l.ReadChar()
+			literal := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.NOT_EQ, Literal: literal}
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
 	case '<':
 		tok = newToken(token.LT, l.ch)
 	case '>':
